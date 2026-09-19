f0c3227210fda8289b69111d67158dcaeae3bebf
Sumit-jha455 <kumarjhasumit171@gmail.com>
2026-09-19 09:55:55 +0530

    Add even odd checker project

---
+++ even_odd_checker.py
@@ -0,0 +1,12 @@
+# Even / Odd Checker
+
+print("===== EVEN / ODD CHECKER =====")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Number:", number)
+    print("Result: EVEN")
+else:
+    print("Number:", number)
+    print("Result: ODD")
